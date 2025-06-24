6ad7cdecdbe229fd24bf25d6ac228ad29c56a91f
SergeySN84 <Sergsvircuk@mail.ru>
2025-06-24 21:21:44 +0700

    Реализация основных модулей

---
+++ src/category.py
@@ -0,0 +1,13 @@
+class Category:
+    # Атрибуты класса для подсчета категорий и товаров
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: list):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        # Обновляем счетчики
+        Category.category_count += 1
+        Category.product_count += len(products)
