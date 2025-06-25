b09540f1f56ddb1b757ab93615ff7fdf9ec17d58
SergeySN84 <Sergsvircuk@mail.ru>
2025-06-25 22:11:27 +0700

    Внесены изменения в тесты, добавлена html отчета о тестировании, добавление файлов __init__.py, добавление в комит poetry.lock.

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,16 +1,5 @@
-import pytest
-from src.product import Product
 from src.category import Category
-
-
-def test_product_initialization():
-    """Тестирует корректную инициализацию объекта Product."""
-    product = Product("Ноутбук", "Мощный игровой ноутбук", 99999.99, 10)
-
-    assert product.name == "Ноутбук"
-    assert product.description == "Мощный игровой ноутбук"
-    assert product.price == 99999.99
-    assert product.quantity == 10
+from src.product import Product
 
 
 def test_category_initialization():
